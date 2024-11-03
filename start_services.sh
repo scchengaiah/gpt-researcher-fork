5facf98e1a9031b57239a7ff01d7d2278d851486
S C Chengaiah <scchengaiah24892@gmail.com>
2024-11-03 11:56:26 +0100

    Updated start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -19,6 +19,25 @@ echo "Installing all dependencies with pip."
 pip install -r requirements.txt
 pip install -r requirements-local.txt
 
+# Ask user if they want to start the frontend
+read -p "Do you want to start the frontend? (y/n): " start_frontend
+
+if [[ $start_frontend =~ ^[Yy]$ ]]; then
+    # Set START_FRONTEND to True in start_services.py
+    sed -i 's/START_FRONTEND = False/START_FRONTEND = True/' start_services.py
+    echo "START_FRONTEND set to True in start_services.py"
+
+    echo "Installing npm dependencies..."
+    cd frontend/nextjs
+    npm install --legacy-peer-deps
+    cd ../..
+    echo "npm dependencies installed."
+else
+    # Set START_FRONTEND to False in start_services.py
+    sed -i 's/START_FRONTEND = True/START_FRONTEND = False/' start_services.py
+    echo "START_FRONTEND set to False in start_services.py"
+fi
+
 # Run the start_services.py script
 echo "Starting services..."
 trap "echo 'Terminating script...'; exit" SIGINT SIGTERM
